a08fdc372189c86a7fd01ce5557e0ad0389c22b8
geldridge13 <73252369+geldridge13@users.noreply.github.com>
2020-12-01 14:55:42 -0500

    pt 2, section 1

--- dom.js
+++ dom.js
@@ -8,7 +8,7 @@ for (let i=0; i<listItems.length; i++) {
 
 //Part 1 Problem 2 
 
-/*let userPrompt = prompt("Enter logo size small or large.")
+let userPrompt = prompt("Enter logo size small or large.")
 let logo = document.getElementById('logo')
 if(userPrompt == "small"){
     logo.setAttribute('width', "400")
@@ -16,7 +16,7 @@ if(userPrompt == "small"){
 } else if (userPrompt == "large") {
     logo.setAttribute("width", "800")
     logo.setAttribute("height", "500")
-}*/
+}
 //Part 1 Problem 3
 
 let userColor=prompt("Pick a color: Red, Blue or Yellow.")
@@ -31,3 +31,24 @@ if (userColor == "red"){
     body[0].style.backgroundColor="yellow"
     
 } 
+
+//Part 1 Problem 4
+
+for(i = 0; i < 5; i++){
+    let team1=prompt("Team member name");
+    teamList[i].textContent = team1;
+}
+
+
+
+function headerCreate() {
+    let h = document.createElement("HEADER");
+    //document.body.appendChild(h);
+    document.body.insertBefore(h,document.body.childNodes[0])
+    let h2 = document.createElement("H2");
+    let txt = document.createTextNode("Header element containing a h2 element is now created");
+    h2.appendChild(txt);
+    h.appendChild(h2);
+}
+headerCreate()
+
