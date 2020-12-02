899c2e27364a0554d31c7b78087a18419b695448
deloris19904 <73267872+deloris19904@users.noreply.github.com>
2020-12-02 13:49:08 -0500

    Completed Part 2 Problem 1

--- dom.js
+++ dom.js
@@ -8,7 +8,7 @@ for (let i=0; i<listItems.length; i++) {
 
 //Part 1 Problem 2 
 
-/*let userPrompt = prompt("Enter logo size small or large.")
+let userPrompt = prompt("Enter logo size small or large.")
 let logo = document.getElementById('logo')
 if(userPrompt == "small"){
     logo.setAttribute('width', "400")
@@ -16,7 +16,8 @@ if(userPrompt == "small"){
 } else if (userPrompt == "large") {
     logo.setAttribute("width", "800")
     logo.setAttribute("height", "500")
-}*/
+}
+
 //Part 1 Problem 3
 
 let userColor=prompt("Pick a color: Red, Blue or Yellow.")
@@ -31,3 +32,23 @@ if (userColor == "red"){
     body[0].style.backgroundColor="yellow"
     
 } 
+// Prompt the user for their name and create a header the
+ // say's `<user's name>'s Resume!` eg. `Kaleb's Resume!`. 
+ // You should use the DOM to create a new element, not to update 
+// an existing element.
+
+// Part 2 Problem 1
+let userName=prompt("Please Enter Your Name")
+function headerCreate(userName){
+    let h=document.createElement("h1")
+    let text=document.createTextNode(userName+"'s Resume!")
+    h.appendChild(text)
+    document.body.appendChild(h)
+}
+headerCreate(userName)
+
+
+
+
+
+
